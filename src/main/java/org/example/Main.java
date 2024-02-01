61f30d68bde464d790f736fe5e3cea50bb624954
Jasa95 <jaco3020@stud.kea.dk>
2024-02-01 13:45:19 +0100

    Working website and filename writer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Indtast webside");
+        System.out.println("Indtast webside med http://");
         String k = sc.nextLine();
         System.out.println("Indtast filnavn");
         String w = sc.nextLine();
