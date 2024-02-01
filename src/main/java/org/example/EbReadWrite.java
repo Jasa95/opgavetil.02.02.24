b349baa470f269a9104fe0e19394cc2b8858c968
Jasa95 <jaco3020@stud.kea.dk>
2024-02-01 13:38:51 +0100

    Working website and filename writer

---
+++ src/main/java/org/example/EbReadWrite.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class EbReadWrite {
+    public void readWrite(String url, String fileName) {
+        try {
+            URL urlEB = new URL(url);
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlEB.openStream()));
+            String inputLine;
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+            while ((inputLine = in.readLine()) != null) {
+                bw.write(inputLine);
+            }
+        } catch (Exception e) {
+    }
+}
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indtast webside");
+        String k = sc.nextLine();
+        System.out.println("Indtast filnavn");
+        String w = sc.nextLine();
+
+        EbReadWrite ebReadWrite = new EbReadWrite();
+        ebReadWrite.readWrite(k, w);
+    }
+
+}
